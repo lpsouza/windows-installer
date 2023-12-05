cca8657d0c4f5005aa9b3dd7bff26c88558563b2
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2023-12-05 12:15:04 -0300

    Changes on generate inventory script

--- generate-inventory.sh
+++ generate-inventory.sh
@@ -5,6 +5,9 @@
 # Get the IP address of the Windows host
 WIN_HOST_IP=$(ip route | grep default | awk '{print $3}')
 
+# Get the IP address of the WSL2 instance
+WSL2_IP=$(ip addr | grep eth0 | grep inet | awk '{print $2}' | cut -d '/' -f 1)
+
 # Get ansible user and password
 read -p "Enter ansible_user: " ANSIBLE_USER
 read -sp "Enter ansible_password: " ANSIBLE_PASSWORD
@@ -19,4 +22,5 @@ ansible_user=$ANSIBLE_USER
 ansible_password=$ANSIBLE_PASSWORD
 ansible_connection=winrm
 ansible_winrm_server_cert_validation=ignore
+wsl2_ip=$WSL2_IP
 EOF
