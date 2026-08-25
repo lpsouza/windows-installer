96482d5fd74bae70c84b08c6aa1b24533aad8b8b
Luiz Souza <lpsouza@gmail.com>
2026-08-24 22:33:23 -0300

    Enforce Network Level Authentication and improve firewall rule in Remote Desktop script

--- scripts/enable-remote-desktop.ps1
+++ scripts/enable-remote-desktop.ps1
@@ -7,26 +7,38 @@ if (-not ([bool]([Security.Principal.WindowsPrincipal] [Security.Principal.Windo
 
 $rdpRegistryPath = "HKLM:\System\CurrentControlSet\Control\Terminal Server"
 $rdpRegistryName = "fDenyTSConnections"
-$currentValue = (Get-ItemProperty -Path $rdpRegistryPath -Name $rdpRegistryName).$rdpRegistryName
+$currentRdpValue = (Get-ItemProperty -Path $rdpRegistryPath -Name $rdpRegistryName -ErrorAction SilentlyContinue).$rdpRegistryName
 
+# Enable Remote Desktop
 Set-ItemProperty -Path $rdpRegistryPath -Name $rdpRegistryName -Value 0
+
+# Enforce Network Level Authentication (NLA)
+$nlaRegistryPath = "HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp"
+$currentNlaValue = (Get-ItemProperty -Path $nlaRegistryPath -Name "UserAuthentication" -ErrorAction SilentlyContinue).UserAuthentication
+Set-ItemProperty -Path $nlaRegistryPath -Name "UserAuthentication" -Value 1
+Set-ItemProperty -Path $nlaRegistryPath -Name "SecurityLayer" -Value 2
 
 $rdpRuleName = "[WindowsInstaller] Allow Remote Desktop (TCP-In)"
 $existingRule = Get-NetFirewallRule -DisplayName $rdpRuleName -ErrorAction SilentlyContinue
+
+$rdpRuleDefinition = @{
+  DisplayName = $rdpRuleName
+  Direction   = "Inbound"
+  Action      = "Allow"
+  Protocol    = "TCP"
+  LocalPort   = 3389
+  Profile     = "Private"
+  Enabled     = "True"
+}
+
 if ($null -eq $existingRule) {
-  New-NetFirewallRule \
-    -DisplayName $rdpRuleName \
-    -Direction Inbound \
-    -Action Allow \
-    -Protocol TCP \
-    -LocalPort 3389 \
-    -Profile Private | Out-Null
+  New-NetFirewallRule @rdpRuleDefinition | Out-Null
 } else {
   Set-NetFirewallRule -DisplayName $rdpRuleName -Enabled True -Profile Private | Out-Null
 }
 
-if ($currentValue -ne 0) {
+if ($currentRdpValue -ne 0 -or $currentNlaValue -ne 1) {
   Restart-Service -Name "TermService" -Force
 }
 
-Write-Host "Remote Desktop is enabled and firewall rule is configured."
+Write-Host "Remote Desktop is enabled with Network Level Authentication (NLA) and firewall rule configured."
